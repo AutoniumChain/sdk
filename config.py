f84a4c6047e59b105d9d5fb93fe66cb93ee6fb6a
Autonium <paymoneyy063@gmail.com>
2026-01-29 11:32:43 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,5 @@
+SURVIVAL_PAYMENT_INTERVAL = 1800  # 30 minutes
+SURVIVAL_PAYMENT_AMOUNT = 0.1     # example amount (ETH/SOL/etc)
+SURVIVAL_ADDRESS = "Autonium_FEE_ADDRESS"
+
+CHAIN = "Solana"  # or solana, base, arbitrum, etc
